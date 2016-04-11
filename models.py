e78c75c63c49d71592f42b48b1f8390cb8361a2f
Braden Jageman <neurobomber@gmail.com>
2016-04-10 23:18:16 -0700

    huh

--- models.py
+++ models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Template(models.Model):
+    active = models.BooleanField(default = False)
+    header = models.CharField(max_length=100)
+    subheader = models.CharField(max_length=300)
+    about1 = models.TextField(max_length=300)
+    about2 = models.TextField(max_length=300)
+    def __unicode__(self):
+        return "%s, Active:%s" % (self.header,self.active)
--- views.py
+++ views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
+from models import Template
 # Create your views here.
 def index(request):
-    args = {}
+    try:
+        template = Template.objects.filter(active=True)[0]
+        args = {"template":template}
+    except IndexError:
+        args = {}
     return render_to_response("landingpage/index.html", RequestContext(request, args))
