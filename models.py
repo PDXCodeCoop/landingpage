f710530330cd69f81dc05161ed32e23970ca02e3
Braden Jageman <neurobomber@gmail.com>
2016-04-10 23:53:50 -0700

    added images

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
+from easy_thumbnails.fields import ThumbnailerImageField
 
 from django.db import models
 
 # Create your models here.
 class Template(models.Model):
+    header_image = ThumbnailerImageField(upload_to='landingpage', blank=True, null=True)
     active = models.BooleanField(default = False)
     header = models.CharField(max_length=100)
     subheader = models.CharField(max_length=300)
